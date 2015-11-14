8ce27302820ece4318ffd169bf469966e378a060
Sharada <ltsharada@gmail.com>
2015-11-14 09:03:46 +0000

    checkpoint_25

--- bloc/code/bloccit/config/routes.rb
+++ bloc/code/bloccit/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+  get 'welcome/index'
   get 'posts/index'
 
   get 'posts/show'
@@ -15,75 +15,9 @@ Rails.application.routes.draw do
   get 'about' => 'welcome#about'
   
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-=======
-  get 'welcome/index'
->>>>>>> checkpoint_25
+  # See how all your routes lay out with "rake routes"
 
   get 'welcome/about'
-
-<<<<<<< HEAD
-<<<<<<< HEAD
   root to: 'welcome#index'
-  #root to: 'welcome#contact'
-  #root to: 'welcome#faq'
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-=======
-   root to: 'welcome#index'
-  #root to: 'welcome#contact'
-  #root to: 'welcome#faq'
-  #root to: 'welcome#about'
->>>>>>> bb2fe0795aebba22a96a6760839bcc152a1d9586
-=======
-  root to: 'welcome#index'
-  
->>>>>>> checkpoint_25
 end
 
