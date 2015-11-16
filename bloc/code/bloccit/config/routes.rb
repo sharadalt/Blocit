42b91f713100db010fb3b849399fc799c60604e3
Sharada <ltsharada@gmail.com>
2015-11-16 17:22:15 +0000

    commit

--- bloc/code/bloccit/config/routes.rb
+++ bloc/code/bloccit/config/routes.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Rails.application.routes.draw do
   resources :posts
   
@@ -61,27 +60,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-=======
-  Rails.application.routes.draw do
-      get 'welcome/index'
-      get 'welcome/about'
-      
-    
-      # The priority is based upon order of creation: first created -> highest priority.
-      # See how all your routes lay out with "rake routes"
-    
-       root to: 'welcome#index'
-       get 'welcome/index'
-       get 'welcome/about'
-       get 'welcome/faq'
-       get 'posts/show'
-       get 'posts/new'
-       get 'posts/edit'
-       
-       resources :posts
-      #root to: 'welcome#contact'
-      #root to: 'welcome#faq'
-      #root to: 'welcome#about'
->>>>>>> master
+ 
 end
 
