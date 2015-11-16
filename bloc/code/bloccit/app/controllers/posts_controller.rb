fd918bab3bafc1f35dccdc158a864b4d000ef786
Sharada <ltsharada@gmail.com>
2015-11-16 19:43:45 +0000

    commit

--- bloc/code/bloccit/app/controllers/posts_controller.rb
+++ bloc/code/bloccit/app/controllers/posts_controller.rb
@@ -1,15 +1,15 @@
 class PostsController < ApplicationController
   def index
    @posts = Post.all
-   @posts.each_with_index do |post, index|
-      if index % 5 == 0
-        post.title = "SPAM"
-      end
-    end
   end
 
   def show
 
+  end
+  
+  def create
+    @post = new()
+    Post.count +=1
   end
 
   def new
