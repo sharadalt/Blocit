65d681c6cadf12a8d8b7d6b0edbf7e6f1cdca5bb
Sharada <ltsharada@gmail.com>
2015-11-23 20:31:39 +0000

    checkpoint_35

--- bloc/code/bloccit/app/helpers/application_helper.rb
+++ bloc/code/bloccit/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def form_group_tag(errors, &block)
+ # #5
+     if errors.any?
+       content_tag :div, capture(&block), class: 'form-group has-error'
+     else
+       content_tag :div, capture(&block), class: 'form-group'
+     end
+   end
 end
