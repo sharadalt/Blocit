ffae1fd3f421ec9374969e759f87593cf4215098
Sharada <ltsharada@gmail.com>
2015-11-24 04:55:42 +0000

    checkpoint_36_assignment

--- bloc/code/bloccit/app/models/user.rb
+++ bloc/code/bloccit/app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
-    before_save { self.email = email.downcase }
+    before_save { self.email = email.downcase 
+                  names = self.name.split(" ")
+                  self.first_name = names[0]
+                  self.last_name  = names[1]
+                  @user.each do |u|
+                      u.first_name.capitalize
+                      u.last_name.capitalize
+                      u.name = u.first_name.join.last_name(" ")
+                  end
+    }
 
  # #3
     EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
