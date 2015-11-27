18a48520ed9055122dfc9eb05d8da4c79266157e
Sharada <ltsharada@gmail.com>
2015-11-27 05:39:11 +0000

    checkpoint_38

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources  :advertisements
   resources  :question
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   post '/users/confirm' => 'users#confirm'
   
   get 'about' => 'welcome#about'
