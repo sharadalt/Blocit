39d9879a16f4f0aaecc44676b766f551204307b5
Sharada <ltsharada@gmail.com>
2015-12-08 19:28:04 +0000

    checkpoint_44

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
   it { should have_many(:posts) }
   it { should have_many(:comments) }
   it { should have_many(:votes) }
+  it { should have_many(:favorites) }
+  
 
   # Shoulda tests for name
   it { should validate_presence_of(:name) }
@@ -101,4 +104,23 @@ RSpec.describe User, type: :model do
     end
  
   end
+  
+  describe "#favorite_for(post)" do
+     before do
+       topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+       @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+     end
+ 
+     it "returns `nil` if the user has not favorited the post" do
+ # #1
+       expect(user.favorite_for(@post)).to be_nil
+     end
+ 
+     it "returns the appropriate favorite if it exists" do
+ # #2
+       favorite = user.favorites.where(post: @post).create
+ # #3
+       expect(user.favorite_for(@post)).to eq(favorite)
+     end
+   end
 end
