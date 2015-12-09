14eaecc25e3354b52da3738e96c9bccd013201a3
Sharada <ltsharada@gmail.com>
2015-12-09 06:56:35 +0000

    checkpoint_46_assignment

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,7 +4,9 @@ class Topic < ActiveRecord::Base
     has_many :labelings, as: :labelable
  # #13
     has_many :labels, through: :labelings
-    scope :visible_to, -> (user) { user ? all : where(public: true) }
+    scope :publically_viewable, -> { where(public: true)}
+    scope :privately_viewable, -> { where(public: false)}
+    scope :visible_to, -> (user) { user ? all : publically_viewable }
     validates :name, length: { minimum: 5 }, presence: true
     validates :description, length: { minimum: 15 }, presence: true
 end
