035f6083efef2eda863fc85762d30a6b18d3fda8
Sharada <ltsharada@gmail.com>
2015-12-09 05:55:35 +0000

    checkpoint_45

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   end
   resources  :advertisements
   resources  :question
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   post '/users/confirm' => 'users#confirm'
   
